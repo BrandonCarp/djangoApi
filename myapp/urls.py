f63f7c38d690f58ab74c2414092adceac303042c
brandon <brancarp93@gmail.com>
2024-12-17 19:35:08 -0500

    donezo

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import fetch_news
 
+
+
 urlpatterns = [
-    path('api/news/', fetch_news, name='fetch_news'),
+    path('api/news', fetch_news, name='fetch_news'),
 ]
